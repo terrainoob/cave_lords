1205eb4d18e4e841fd0bf92df03a62205cffd177
terrainoob <terrainoob@gmail.com>
2022-12-30 07:38:06 -0500

    include dragonruby engine

--- mygame/app/main.rb
+++ mygame/app/main.rb
@@ -1,5 +1,11 @@
 # this is where things start when ./dragonruby is executed
 
 def tick(args)
-  args.outputs.labels << [640, 540, 'Hi there!', 5, 1]
+  x = 640
+  y = 540
+  text = 'Hi there!'
+  size = 5
+  align = 1
+
+  args.outputs.labels << [x, y, text, size, align]
 end
