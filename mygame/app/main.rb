cc7430b6901f14b13672a397e4f7de2c7973af40
terrainoob <terrainoob@gmail.com>
2022-12-29 23:42:58 -0500

    more converting to dragon

---
+++ mygame/app/main.rb
@@ -0,0 +1,5 @@
+# this is where things start when ./dragonruby is executed
+
+def tick(args)
+  args.outputs.labels << [640, 540, 'Hi there!', 5, 1]
+end
--- spec/game/game_window_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe GameWindow do
-  describe '.run' do
-    it 'does something' do
-      expect(1+1).to eq 2
-    end
-  end
-end
