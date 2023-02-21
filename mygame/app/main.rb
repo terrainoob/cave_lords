91a3314a5159af2a5f1b41f44ffb026633982cb8
terrainoob <terrainoob@gmail.com>
2023-02-21 17:24:01 -0500

    make tile info window stationary and prettier

--- mygame/app/main.rb
+++ mygame/app/main.rb
@@ -13,35 +13,23 @@ def tick(args)
   setup(args) if args.tick_count.zero?
   Menu.tick(args)
   assign_world_map_sprite(args)
-
-  if args.inputs.mouse.button_left
-    x = args.inputs.mouse.x
-    y = args.inputs.mouse.y
-    tile_x = (x / World.instance.tile_size).floor
-    tile_y = (y / World.instance.tile_size).floor
-    tile = World.instance.tiles[tile_x][tile_y]
-    tile_info_window(x, y, tile, args)
-  end
+  tile_info_window(args.inputs.mouse.x, args.inputs.mouse.y, args)
 end
 
-def tile_info_window(x, y, tile, args)
-  box =
-    {
-      x: x,
-      y: y,
-      w: 300,
-      h: 250,
-      r: 100,
-      g: 100,
-      b: 100,
-      a: 200,
-      primitive_marker: :solid
-    }
-  args.outputs.primitives << box
-  args.outputs.labels << { x: x + 10, y: y + 50, text: "Biome: #{tile.biome}" }
-  args.outputs.labels << { x: x + 10, y: y + 100, text: "Temperature: #{tile.temperature_value.floor} C" }
-  args.outputs.labels << { x: x + 10, y: y + 150, text: "Height Value: #{tile.height_value.round(2)}" }
-  args.outputs.labels << { x: x + 10, y: y + 200, text: "Precipitation: #{(tile.moisture_value * 100).floor}%" }
+def tile_info_window(x, y, args)
+  return if x > 1280 || y > 720
+
+  tile_x = (x / World.instance.tile_size).floor
+  tile_y = (y / World.instance.tile_size).floor
+
+  return unless World.instance.tiles[tile_x][tile_y]
+
+  tile = World.instance.tiles[tile_x][tile_y]
+  args.outputs.primitives << { x: 980, y: 0, w: 300, h: 250, r: 150, g: 150, b: 150, a: 200, primitive_marker: :solid }
+  args.outputs.labels << { x: 990, y: 200, text: "Biome: #{tile.biome}" }
+  args.outputs.labels << { x: 990, y: 100, text: "Temperature: #{tile.temperature_value.floor} C" }
+  args.outputs.labels << { x: 990, y: 150, text: "Height Value: #{tile.height_value.round(2)}" }
+  args.outputs.labels << { x: 990, y: 50, text: "Precipitation: #{(tile.moisture_value * 100).floor}%" }
 end
 
 def assign_world_map_sprite(args)
