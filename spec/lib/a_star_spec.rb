c34c65a719594106ed6dbb12b20c82912a4fc8df
terrainoob <rubberduckdevshow@gmail.com>
2023-07-04 12:54:09 -0400

    fix some of the stupid in my a_star test

--- spec/lib/a_star_spec.rb
+++ spec/lib/a_star_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe AStar do
   let(:a_star) { AStar.new }
-  let(:map) { WorldMap.new(width: 5, height: 5, tile_size: 10, seed: 123) }
-  let(:start_tile) { map[0, 0] }
-  let(:end_tile) { map[3, 4] }
+  let(:world_map) { WorldMap.new(width: 5, height: 5, tile_size: 10, seed: 123).map }
+  let(:start_tile) { world_map[0, 0] }
+  let(:end_tile) { world_map[3, 4] }
 
   # 4 - - - - -
   # 3 - - - - -
@@ -15,11 +15,12 @@ describe AStar do
 
   it 'returns a path from the start_tile to the destination_tile' do
     skip
-    path = a_star.find_path(start_tile, end_tile)
-    expect(path).to contain(map[0, 0])
-    expect(path).to contain(map[1, 1])
-    expect(path).to contain(map[2, 2])
-    expect(path).to contain(map[3, 3])
-    expect(path).to contain(map[3, 4])
+    path = a_star.find_path(start_tile, end_tile, world_map)
+
+    expect(path).to include(world_map[0, 0])
+    expect(path).to include(world_map[1, 1])
+    expect(path).to include(world_map[2, 2])
+    expect(path).to include(world_map[3, 3])
+    expect(path).to include(world_map[3, 4])
   end
 end
