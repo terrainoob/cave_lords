fb76f97bef52d7773c3d66ca562a16b326db6956
terrainoob <rubberduckdevshow@gmail.com>
2023-08-01 17:35:43 -0400

    setup to gen the play map properly

--- mygame/app/game/models/maps/play_map.rb
+++ mygame/app/game/models/maps/play_map.rb
@@ -1,13 +1,15 @@
 class PlayMap < GenericMap
+  def initialize(width:, height:, tile_size:, seed:, selected_world_tile:)
+    @selected_world_tile = selected_world_tile
+    super(width: width, height: height, tile_size: tile_size, seed: seed)
+  end
+
   def generate_map
-    map = []
-    (0...@width).each do |x|
-      map[x] = []
-      (0...@height).each do |y|
-        tile = PlayMapTile.new(x: x, y: y, size: @tile_size)
-        map[x][y] = tile
-        @sprites << tile.sprite
-      end
+    if @selected_world_tile
+      # select tileset based on biome
+      # use wfc to generate random map from that tileset.
+    else
+      puts "YOU DON'T HAVE A SELECTED WORLD TILE IN THE PLAYMAP GEN!!!  STUPID!!!"
     end
   end
 end
